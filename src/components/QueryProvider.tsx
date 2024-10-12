611d641ddb11aa088fe408201b599b2c6864fb84
aungkoko-shopdoora <aungkoko.mhs@gmail.com>
2024-10-12 10:47:30 +0630

    init

---
+++ src/components/QueryProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const QueryProvider = ({ children }: React.PropsWithChildren) => {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
